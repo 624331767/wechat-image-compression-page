6f6068db459733f67bc8b01634264fffb48dfc8d
username <624331767@qq.com>
2025-10-25 18:23:47 +0800

    build(assets): 更新 About 页面的 JavaScript 文件哈希值
    将 `About-5007c58a.js` 重命名为 `About-698e675d.js`，并更新其中引用的依赖文件版本，
    以确保资源正确加载。此变更属于构建过程的一部分，用于维持前端资源的版本一致性。

--- public/dist/assets/index-bfd61373.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-481431e1.js";const c={},n=window.Vue.openBlock,o=window.Vue.createElementBlock;function t(r,_){return n(),o("div")}const a=e(c,[["render",t]]);export{a as default};
