ead4a0456e6408dd8c28bdb2144a64c52ffd67b9
username <624331767@qq.com>
2025-11-04 11:53:55 +0800

    ``` build(assets): 删除多个未使用的图片资源文件
    清理 public/dist/assets 目录中不再需要的二进制图片文件，
    包括 PNG 和 JPG 格式的多个版本图像资源。
    ```

--- public/dist/assets/js-index-banner_1-8730cc28.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("js-index-banner_1-44a3d5c1.jpg",import.meta.url).href;export{e as _};
--- public/dist/assets/lishuaweidianqian-687d4f5f.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("lishuaweidianqian-7e0ede38.jpg",import.meta.url).href;export{e as _};
